74b3f382dee9d3154567592f66d436f7de5a9584
greyewi1 <greyewi@gmail.com>
2021-05-08 18:26:25 +0500

    add Image container

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,67 +1,49 @@
-import React, {memo, useCallback, useMemo} from 'react'
-import { Button } from 'react-bootstrap';
-import '../assets/gameList.scss';
-import noCover from '../assets/nocover.png';
+import React, {memo, Suspense, useCallback, useMemo} from 'react'
+import {Button} from 'react-bootstrap'
+import '../assets/gameList.scss'
+import noCover from '../assets/nocover.png'
+import {Img} from 'react-image'
 
+function MyImageComponent({src}) {
+  const url = src && "https" + src.slice(4)
+  return <div>
+    <Img
+      src={url}
+      container={() => {
+        return <img className="gameCover" src={url}/>
+      }}
+      loader={<div>Loading...</div>}
+      unloader={<img className="gameCover" src={noCover}/>}
+    />
+  </div>
+}
 
 const GameCard = (props) => {
-    const { item, objkey, addToWishList, removeFromWishListAction, isWishList } = props;
+  const {item, objkey, addToWishList, removeFromWishListAction, isWishList} = props
 
-    const onClickBtn = useCallback((item, objkey) => {
-      !isWishList ? addToWishList({ item, objkey }) : removeFromWishListAction({...item, key: objkey})
-    }, [isWishList, addToWishList, removeFromWishListAction])
+  const onClickBtn = useCallback((item, objkey) => {
+    !isWishList ? addToWishList({item, objkey}) : removeFromWishListAction({...item, key: objkey})
+  }, [isWishList, addToWishList, removeFromWishListAction])
 
-    const btnText = useMemo(() => !isWishList ? "Add to List" : 'Remove', [isWishList])
+  const btnText = useMemo(() => !isWishList ? "Add to List" : 'Remove', [isWishList])
+  return (
+    <div className="gameCard">
+      {/*<img className="gameCover" src={item.cover} />*/}
 
-    try{
-      return (
-        <div className="gameCard">
-            <img className="gameCover" src={item.cover} />
-          <Button
-            className='gameBtn'
-            onClick={() => onClickBtn(item, objkey)}
-          >
-            {btnText}
-          </Button>
+        <MyImageComponent src={item.cover}/>
 
-          <span className='info'>{item?.name || ' '}</span>
-          <span className='info'>{`${item?.price || '0'} rub.`}</span>
-        </div>
-      )
-    } catch(err){
-      console.log(err)
-      return (
-        <div className="gameCard">
-            <img className="gameCover" src={noCover} />
-          <Button
-            className='gameBtn'
-            onClick={() => onClickBtn(item, objkey)}
-          >
-            {btnText}
-          </Button>
+      <Button
+        className='gameBtn'
+        onClick={() => onClickBtn(item, objkey)}
+      >
+        {btnText}
+      </Button>
 
-          <span className='info'>{item?.name || ' '}</span>
-          <span className='info'>{`${item?.price || '0'} rub.`}</span>
-        </div>
-      )
-    }
+      <span className='info'>{item?.name || ' '}</span>
+      <span className='info'>{`${item?.price || '0'} rub.`}</span>
+    </div>
+  )
+
 }
 
-const noWishList = (prevProps, nextProps) => {
-
-  if(prevProps.wishListData.length !== nextProps.wishListData.length){
-    if(!!nextProps.wishListData.find(f => f.key === nextProps.objkey)){
-      return false
-    }
-
-    if(prevProps.wishListData.find(f => f.key === prevProps.objkey) && !nextProps.wishListData.find(f => f.key === nextProps.objkey)){
-      return false
-    }
-
-    return true
-  }
-
-  return false
-}
-
-export default memo(GameCard, noWishList);
+export default GameCard
